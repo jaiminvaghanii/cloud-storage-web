f9e4437c989a5009aa0a3c46d42a5a5531e8c9bc
Jaimin Vaghani <jaiminvaghani.vj@gmail.com>
2025-12-26 21:26:23 +0530

    removed lenis

--- src/components/layout/ScrollToTop.tsx
+++ src/components/layout/ScrollToTop.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Lenis from 'lenis';
+// import Lenis from 'lenis';
 
 export const ScrollToTop = () => {
     const { pathname } = useLocation();
